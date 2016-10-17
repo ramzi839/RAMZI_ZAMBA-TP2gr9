edea5b9d4dcd5f7bc81382a827e9af6f2c301d38
mouad <ramzi.mouad@outlook.com>
2016-10-17 14:22:10 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,7 @@
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
-
-void testSFML() {
-    sf::Texture texture;
-}
+#include <SFML/Window.hpp>
 
 // Fin test SFML
 
@@ -13,10 +10,50 @@ void testSFML() {
 
 using namespace std;
 
-int main(int argc,char* argv[]) 
+int main()
 {
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RectangleShape background(sf::Vector2f(800,600));	
+    sf::RectangleShape goku_image(sf::Vector2f(100,90));
+    sf::Texture backgroundTexture[5];
+    sf::Texture goku_pic;
+    backgroundTexture[0].loadFromFile("../res/Background/Map0.png");
+    backgroundTexture[1].loadFromFile("../res/Background/Map1.png");
+    backgroundTexture[2].loadFromFile("../res/Background/Map2.png");
+    backgroundTexture[3].loadFromFile("../res/Background/Map3.png");
+    backgroundTexture[4].loadFromFile("../res/Background/Map4.png");
+    int i=0;
+    background.setTexture(&backgroundTexture[0]);
+    window.setFramerateLimit(2);	
+	goku_pic.loadFromFile("../res/Players/sangoku_profil.jpg");
+	goku_image.setTexture(&goku_pic);
+	//goku_pic.loadFromImage(&goku,rect=IntRect());
+	
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+           
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-    cout << "It works !" << endl;
+        
+        window.clear(sf::Color::Blue);
+	if ( i<5) { background.setTexture(&backgroundTexture[i]); i++;}
+	else i=0;
+
+         window.draw(background);
+         window.draw(goku_image);
+		
+        
+        window.display();
+
+    }
 
     return 0;
 }
--- src/state.hpp
+++ src/state.hpp
@@ -1,6 +1,13 @@
 #ifndef __STATE_HPP__
 #define __STATE_HPP__
 
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <fstream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include "state.h"
 
 #endif
