3831bf61f1dbf663d75e89fa03c4bb65bdaaa89c
Fady <fady.zamba-zamba@ensea.fr>
2016-10-17 14:15:59 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 void testSFML() {
     sf::Texture texture;
@@ -16,7 +17,6 @@ using namespace std;
 int main(int argc,char* argv[]) 
 {
 
-    cout << "It works !" << endl;
-
+    //cout << "It works !!!" << endl;
     return 0;
 }
